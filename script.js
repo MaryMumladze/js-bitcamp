f95cb640b76a0a6bd916cd58e72932439efc97ed
MaryMumladze <mmumladzemari12@gmail.com>
2023-10-27 15:54:17 +0400

    js bitcamp

---
+++ script.js
@@ -0,0 +1,37 @@
+let speach = "hellow world!"
+console.log(speach);
+
+let number = 23
+console.log(number);
+
+let firstnumber = 20
+let secondnumber = 55
+console.log(firstnumber+secondnumber);
+
+let firstnumber1 = 40
+let secondnumber2 = 24
+console.log(firstnumber1-secondnumber2);
+
+let number1 = 30
+let number2 = 3
+console.log(number1/number2);
+
+let number3 = 45
+let number4 = 40 
+console.log(number3*number4);
+
+
+console.log("mary" +" "+ "mumladze")
+
+console.log("bitcamp".toUpperCase())
+
+console.log("BITCAMP".toLowerCase())
+
+// console.log("hellow!, " + prompt("what is your name?") )?
+
+let text = "a neline \"character\" is writter like.";
+console.log(text);
+
+// console.log(prompt("This is Bitcamp."))
+
+// console.log(prompt("hellow, world!".toUpperCase()))
