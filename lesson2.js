9befcdd17f7b43b6d8de26759c1ec90cb6c69c00
MaryMumladze <mmumladzemari12@gmail.com>
2023-11-03 19:13:58 +0400

    add js file0

---
+++ lesson2.js
@@ -0,0 +1,17 @@
+let theNumber = Number(prompt("Enter the student's score"));
+
+if(theNumber > 90 && theNumber < 100){
+    console.log("You got an A")
+}
+if(theNumber > 80 && theNumber < 89){
+    console.log("you got an B")
+}
+if(theNumber > 70 && theNumber < 79){
+    console.log("you got an C")
+}
+if(theNumber > 60 && theNumber < 69){
+    console.log("you got an D")
+}
+if(theNumber > 0 && theNumber < 59){
+    console.log("you got an F")
+}
