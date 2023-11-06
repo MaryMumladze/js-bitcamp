d83f5eb25ebca81cdded6becb905064ec7b4db27
MaryMumladze <mmumladzemari12@gmail.com>
2023-11-06 17:58:00 +0400

    add

--- lesson2.js
+++ lesson2.js
@@ -1,17 +1,24 @@
-let theNumber = Number(prompt("Enter the student's score"));
+// let theNumber = Number(prompt("Enter the student's score"));
 
-if(theNumber > 90 && theNumber < 100){
-    console.log("You got an A")
-}
-if(theNumber > 80 && theNumber < 89){
-    console.log("you got an B")
-}
-if(theNumber > 70 && theNumber < 79){
-    console.log("you got an C")
-}
-if(theNumber > 60 && theNumber < 69){
-    console.log("you got an D")
-}
-if(theNumber > 0 && theNumber < 59){
-    console.log("you got an F")
-}
+// if(theNumber > 90 && theNumber < 100){
+//     console.log("You got an A")
+// }
+// if(theNumber > 80 && theNumber < 89){
+//     console.log("you got an B")
+// }
+// if(theNumber > 70 && theNumber < 79){
+//     console.log("you got an C")
+// }
+// if(theNumber > 60 && theNumber < 69){
+//     console.log("you got an D")
+// }
+// if(theNumber > 0 && theNumber < 59){
+//     console.log("you got an F")
+// }
+
+let a = Number(prompt("Enter first number"));
+let b = Number(prompt("Enter second number"));
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
