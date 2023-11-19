2098d1b95d64698ff153c00a9f843c43c6a3aa75
MaryMumladze <mmumladzemari12@gmail.com>
2023-11-19 12:12:33 +0400

    add to Area of a Rectangular Room

--- lesson2.js
+++ lesson2.js
@@ -1,3 +1,4 @@
+// Mad Libs
 // let verb = prompt("enter a verb");
 // let noun = prompt("enter a noun");
 // let adverb = prompt("enter an adverb");
@@ -5,6 +6,8 @@
 
 // document.write("Do you " +  verb + " your " +  adjactive  + " " + noun + " " + adverb + "?" + " That's hilarious!");
 
+
+// Simple Math
 // let theNumber = Number(prompt("Enter the student's score"));
 
 // if(theNumber > 90 && theNumber < 100){
@@ -23,6 +26,8 @@
 //     console.log("you got an F")
 // }
 
+
+// Simple Math
 // let a = Number(prompt("Enter first number"));
 // let b = Number(prompt("Enter second number"));
 // alert(a + b);
@@ -31,14 +36,29 @@
 // alert(a / b);
 
 
+
+
+// Tip Calculator 
 // const a = prompt("How much was the meal?");
 // const b = prompt("What percentage would you like to tip?");
 
 // console.log(a * b / 100);
 
-const a = prompt("What is your current age?");
-const b = prompt("At what age would you like to retire?");
 
-console.log(b - a + 2023);
+
+// Retirement Calculator 
+// const a = prompt("What is your current age?");
+// const b = prompt("At what age would you like to retire?");
+
+// console.log(b - a + 2023);
+
+
+// Area of a Rectangular Room
+const m2 = prompt("what is e lenght of the room in feet?");
+const f2 = prompt("what is the width of the room in feet?");
+const e = 0.09290304
+
+console.log(m2 * f2)
+console.log(m2 * f2 * e);
 
 
